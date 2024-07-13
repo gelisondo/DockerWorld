184043a214f9c50bfbacf696d2b76ef615dc0217
1world 2geeks <1world2geeks@gmail.com>
2024-07-12 22:44:53 -0300

    Agregamos una aplicación en flesk para brindar una Api que solo devuelve un json, el docker file baja una versión con docker con python y corre un comando para instalar los requerimientos(librerias) para nuestra app API. entre otras cosas

---
+++ Api-Python/app.py
@@ -0,0 +1,24 @@
+import json
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/getMyInfo')
+def getMyInfo():
+    value = {
+        "name": "Amin",
+        "lastname": "Espinoza",
+        "socialMedia":
+        [
+            {"facebookUser": "aminespinoza10"},
+            {"instagramUser": "aminespinoza10"},
+            {"xUser": "aminespinoza"},
+            {"linkedin": "amin-espinoza"},
+            {"githubUser": "aminespinoza10"}
+        ],
+        "blog": "https://aminespinoza.com",
+        "author": "Miranda Espinoza"
+    }
+    return json.dumps(value)
+
+if __name__ == "__main__":
+    app.run(port=5000)
