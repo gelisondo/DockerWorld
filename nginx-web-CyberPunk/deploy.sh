68a312e6296a6a8be8bb81dba20604f7869121a3
Un mundo para los Geeks <1world2geeks@gmail.com>
2024-08-21 09:29:08 -0300

    Nos aseguramos que el nombre de imagen/ct y el tag, no existan para crearlos y si existen se elimina la imagen previa, agregamos comentarios de uso del script

--- nginx-web-CyberPunk/deploy.sh
+++ nginx-web-CyberPunk/deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+echo "##############################################################################################################"
+echo "# You can execute this script with 2 atributtes, the firs pass the name IMAGE/CT and the second pass the TAG #"
+echo "# If you dont pass arguments when execute this script, use the default name (wcpunk) and tag (1.0)           #"
+echo "# If you want to pass the expecific port to comunicate with docker add the thert argument, example 8082      #"
+echo "# for default use 8081                                                                                       #"
+echo "# Example: ./deploy imagename tagname 8083                                                                   #"
+echo "##############################################################################################################"
+sleep 2
+
+
 UID=`id -u`
 
 if [ $UID -nq 0 ];
 then
-    echo "You need a id 0 to run this script"
+    echo "You need root privileges to run this script"
     sleep 2
     exit 0
 fi
+
 
 #VERIFICAMOS QUE EXISTAN ARGUMENTOS SI NO ES ASÍ UTILIZAREMOS NOMBRES POR DEFECTO.
 if [ -z $($1) ];
@@ -32,27 +43,38 @@ else
 fi
 
 
-# Make the images with dockerfile instructions
-docker build -t IMAGENAME:IMAGETAG .
-
-# Run docker images to make a container IMAGENAME
-docker run -it --rm -d -p 8081:80 --name IMAGENAME IMAGENAME:IMAGETAG 
-
-
 #Alocate all CT name, when run state.
-CTRUN=`docker images | cut -d " " -f 1 | grep -v "REPOSITORY"`
+#Filtramos el nombre de la imagen con un grep y agregamos otro grep para filtrar el TAG. De esta manera obtenemos la imagen puntual que queremos recrear.
+#Este doble filtro se aplica por que es posible que tengamos una imagen con el mismo nombre pero con diferente TAG.
 
-for ctname in $(echo $CTRUN)
-do
+OSIMAGES=`docker images | grep $IMAGENNAME | grep $IMAGETAG`
+OSIMAGEFILTER=`echo $OSIMAGES | cut -d " " -f 1`
+OSIMAGEFILTERTAG=`echo $OSIMAGES | cut -d " " -f 2`
 
-    if [ $IMAGENAME -eq $ctname ];
-    then
+if [ $OSIMAGEFILTER -z ];
+then
+    echo "The imagen name or tag don't exist, generate new imagens without delete previus version"
+    echo ""
+    echo "Make the images with dockerfile instructions"
+    sleep 1
+    docker build -t IMAGENAME:IMAGETAG .
 
-        echo "Delete this images: $ctname"
-        docker rmi $IMAGENAME:$IMAGETAG
+    echo "Run docker images to make a container IMAGENAME"
+    sleep 1
+    docker run -it --rm -d -p 8081:80 --name IMAGENAME IMAGENAME:IMAGETAG 
 
-    fi
+else
+    echo "Delete this images: $OSIMAGEFILTER"
+    docker rmi $OSIMAGEFILTER:$OSIMAGEFILTERTAG
+
+    echo "Make the images with dockerfile instructions"
+    sleep 1
+    docker build -t IMAGENAME:IMAGETAG .
 
 
-done 
+    echo "Run docker images to make a container IMAGENAME"
+    sleep 1
+    docker run -it --rm -d -p 8081:80 --name IMAGENAME IMAGENAME:IMAGETAG 
+fi
+
 
